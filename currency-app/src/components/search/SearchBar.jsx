002f2b65d4c9d9c9b5d4dd09f0714e51bd9b7c53
AlexanderRoth98 <alexander.roth1998@gmail.com>
2023-01-28 20:09:44 +0100

    search bar prototype

--- currency-app/src/components/search/SearchBar.jsx
+++ currency-app/src/components/search/SearchBar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const SearchBar = () => {
-  return <div id='search-bar'>SearchBar</div>
+  return (
+    <div id='search-bar' className='flex py-2 sticky top-0 bg-blue'>
+      <h2 className='text-2xl ml-4'>Search</h2>
+      <input className='w-4/5 ml-6' type='text' id='fname' name='fname' />
+    </div>
+  )
 }
 
 export default SearchBar
